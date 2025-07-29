151469f166f2835b35d9a519b84bb4a646b79b1d
Taciano <taciolb@gmail.com>
2025-07-29 20:10:22 -0300

    commit inicial com esqueleto

---
+++ src/test/java/com/tlbtech/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlbtech.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
